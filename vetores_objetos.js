2cf8564fdd0a23454161509454d8e0d61c615924
Tiago Lunardi <tiagaoalb@gmail.com>
2020-06-15 14:01:09 -0300

    added vetores e objetos

---
+++ vetores_objetos.js
@@ -0,0 +1,10 @@
+const programador = {
+  nome: "Tiago",
+  idade: 37,
+  tecnologias: [
+    { nome: "C++", especialidade: "Desktop" },
+    { nome: "Javascript", especialidade: "Web/Mobile" },
+  ],
+};
+
+console.log(`O usuário ${programador.nome} tem ${programador.idade} anos e usa a tecnologia ${programador.tecnologias[1].nome} com especialidade em ${programador.tecnologias[1].especialidade}`)
