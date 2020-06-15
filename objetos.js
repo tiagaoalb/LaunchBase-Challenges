ee9d2c20d391e78304129872807cbc0e0997acbf
Tiago Lunardi <tiagaoalb@gmail.com>
2020-06-15 13:47:33 -0300

    added objetos

---
+++ objetos.js
@@ -0,0 +1,11 @@
+const empresa = {
+  Nome: 'RocketSeat',
+  Cor: 'Roxo',
+  Foco: 'Programação',
+  Endereço: {
+    Rua: 'Rua Guilherme Gembala',
+    Número: 260
+  }
+}
+
+console.log(`A empresa ${empresa.Nome} está localizada em ${empresa.Endereço.Rua}, ${empresa.Endereço.Número}`)
